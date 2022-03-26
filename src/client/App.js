4c0d1ed664b05d7dee5f78f978bcb7a8f18f4083
Robert Finkley <rfinkley82@gmail.com>
2022-03-26 18:40:00 -0400

    adding post form

--- src/client/App.js
+++ src/client/App.js
@@ -19,10 +19,27 @@ const initialPosts = [
   },
 ];
 
+const handleSubmit = () => {
+  console.log('Done');
+}
+
 const App = () => {
   const [posts, setPosts] = useState(initialPosts);
+  const [postContent, setPostContent] = useState("");
   return (
     <div className="container">
+      <div className="postForm">
+        <form onSubmit={handleSubmit}>
+          <textarea
+            value={postContent}
+            onChange={(e) => {
+              setPostContent(e.target.value);
+            }}
+            placeholder="Write your custom post!"
+          />
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
       <div className="feed">
         {posts.map((post, i) => (
           <div key={post.id} className="post">
