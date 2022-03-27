4cde932819f4305c6bfabedc99b9ba447822d536
Robert Finkley <me@robertfinkley.com>
2022-03-27 09:03:18 -0400

    adding listener to express app

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,9 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 
 app.get('*', (req, res) => {
-    res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
-app.listen(8000)
+app.listen(8000, () => console.log('Listening on port 8000!'));
