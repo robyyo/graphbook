8f548294726247c7b1f5f3fa7b069b67e2415b79
Robert Finkley <rfinkley82@gmail.com>
2022-03-26 21:38:36 -0400

    added form submit event

--- src/client/App.js
+++ src/client/App.js
@@ -20,8 +20,18 @@ const initialPosts = [
 ];
 
 const handleSubmit = () => {
-  console.log('Done');
-}
+  event.preventDefault();
+  const newPost = {
+    id: posts.length + 1,
+    text: postContent,
+    user: {
+      avatar: "/uploads/avatar1.png",
+      username: "Fake User",
+    },
+  };
+  setPosts([newPost, ...posts]);
+  setPostContent("");
+};
 
 const App = () => {
   const [posts, setPosts] = useState(initialPosts);
