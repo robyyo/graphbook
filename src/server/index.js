2010b613c517f145a73c0bdd706cde55d839d01f
Robert Finkley <me@robertfinkley.com>
2022-03-27 10:54:30 -0400

    added helmet options and compression

--- src/server/index.js
+++ src/server/index.js
@@ -1,14 +1,30 @@
 import express from 'express';
 import path from 'path';
+import helmet from 'helmet'; // Helmet is a tool that allows you to set various HTTP headers to help secure your application.
+import cors from 'cors';
+import compress from 'compression';
 
 const app = express();
 
 const root = path.join(__dirname, '../..');
 
-//the use function, which runs a series of commands when a given path matches. 
+//the use function, which runs a series of commands when a given path matches.
 //When executing this function without a path, it is executed for every request.
 app.use('/', express.static(path.join(root, 'dist/client')));
 app.use('/uploads', express.static(path.join(root, 'uploads')));
+app.use(helmet());
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+      scriptSrc: ["'self'", "'unsafe-inline'"],
+      styleSrc: ["'self'", "'unsafe-inline'"],
+      imgSrc: ["'self'", 'data:', '*.amazonaws.com'],
+    },
+  })
+);
+app.use(helmet.referrerPolicy({ policy: 'same-origin' }));
+app.use(compress());
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(root, '/dist/client/index.html'));
