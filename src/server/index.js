7d89d7bae70451990c030f0980f202457b397d43
Robert Finkley <me@robertfinkley.com>
2022-03-27 09:37:24 -0400

    building on express app

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,17 @@
 import express from 'express';
+import path from 'path';
 
 const app = express();
 
-app.get('*', (req, res) => {
-  res.send('Hello World!');
+const root = path.join(__dirname, '../..');
+
+//the use function, which runs a series of commands when a given path matches. 
+//When executing this function without a path, it is executed for every request.
+app.use('/', express.static(path.join(root, 'dist/client')));
+app.use('/uploads', express.static(path.join(root, 'uploads')));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(root, '/dist/client/index.html'));
 });
 
 app.listen(8000, () => console.log('Listening on port 8000!'));
