1c3a39abe19ae3725fa6c1e19b46ef9f7ba332d5
Robert Finkley <me@robertfinkley.com>
2022-04-01 20:34:38 -0400

    added RootMutation

--- src/server/services/graphql/resolvers.js
+++ src/server/services/graphql/resolvers.js
@@ -1,4 +1,4 @@
-const posts = [
+let posts = [
   {
     id: 2,
     text: 'Lorem ipsum',
@@ -20,6 +20,17 @@ const resolvers = {
   RootQuery: {
     posts(root, args, context) {
       return posts;
+    },
+  },
+  RootMutation: {
+    addPost(root, { post, user }, context) {
+      const postObject = {
+        ...post,
+        user,
+        id: posts.length + 1,
+      };
+      posts.push(postObject);
+      return postObject;
     },
   },
 };
